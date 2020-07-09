824bcb3cd9027faa6e369298e01c4e80e89d6d1e
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-07-09 15:22:15 -0400

    created routes

---
+++ routes/api/index.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const router = require("express").Router();
+const bookRoutes = require("./books");
+const googleRoutes = require("./google");
+
+// Book routes
+router.use("/books", bookRoutes);
+
+// Google Routes
+// router.use("/google", googleRoutes);
+// router.use("/api/google", googleRoutes);
+
+
+// For anything else, render the html page
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+module.exports = router;

767f06d1a1bfc62e7177a07a3d8368cfc8f1fddc
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-07-09 15:02:54 -0400

    created model

--- models/book.js
+++ models/book.js
@@ -1,20 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-//Construction of book schema
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  subtitle:{type:String} ,
-  author: { type: Array, required: true },
+  subtitle: { type: String },
+  authors: { type: [String], required: true },
+  link: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
-  link: { type: String, required: true }, 
-  // date: { type: Date, default: Date.now },
-  // googleId: { type: String, required: true, unique: true }
+  googleId: { type: String, required: true, unique: true }
 });
 
 const Book = mongoose.model("Book", bookSchema);
 
 module.exports = Book;
-
-
